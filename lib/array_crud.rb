8f50ccd623730e8402cc5cc34535981b8401da65
Corey Martin <coreyjmartin2@gmail.com>
2020-08-24 03:34:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [[1, 2, 3, 4]
-  #{element
+  array = ["wow", "I", "am", "really", "learning"]
+  #{element} << array
 end
 
 def add_element_to_start_of_array(array, element)
